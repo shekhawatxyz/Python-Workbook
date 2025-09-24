cd526a7eff383aa931e1d8561d1e7669c80598fb
Parakram <94182022+shekhawatxyz@users.noreply.github.com>
2025-09-24 16:10:38 +0530

    till exercise 38, chapter 2

---
+++ Chapter-02-Decision-Making/Exercise-38-Even-or-Odd.py
@@ -0,0 +1,8 @@
+while True:
+    a = int(input("Enter an integer: "))
+    if a % 2 == 0:
+        print("This is an even number")
+        break
+    else:
+        print("This is an odd number")
+        break
